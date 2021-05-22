51e47ae8c95921089677d18f31d7b5084df1f6ec
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-22 23:51:28 +0300

    create doctor login serializer and view

--- doctor/urls.py
+++ doctor/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import DoctorRegistrationView
 from .views import DoctorActivationView
+from .views import DoctorLoginView
 
 urlpatterns = [
     path('doctor/new', DoctorRegistrationView.DoctorCreate.as_view()),
+    path('doctor/login', DoctorLoginView.DoctorLoginView.as_view()),
     path('doctor/<int:pk>/activate', DoctorActivationView.DoctorActivation.as_view())
 ]
 
