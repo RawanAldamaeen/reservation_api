a753a4eb887085ab25d1786d9c7db403b2b61a79
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-23 07:28:39 +0300

    create patient login serializer and view

--- patient/urls.py
+++ patient/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import PatientRegistration
 from .views import PatientActivationView
+from .views import PatientLoginView
 
 urlpatterns = [
     path('patient/new', PatientRegistration.PatientCreate.as_view()),
     path('patient/user-verify', PatientActivationView.VerifyUser.as_view(), name="user-verify"),
+    path('patient/login', PatientLoginView.PatientLoginView.as_view()),
 
 ]
 
---
+++ patient/views/PatientLoginView.py
@@ -0,0 +1,23 @@
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from ..serializers.PatientLoginSerializer import PatientLoginSerializers
+from django.contrib.auth.models import update_last_login
+from rest_framework.response import Response
+from rest_framework import status
+from base.models.user import User
+
+
+class PatientLoginView(generics.GenericAPIView):  # Patient login view
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = PatientLoginSerializers(data=request.data, context={'request': request})
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            data={"status": status.HTTP_422_UNPROCESSABLE_ENTITY, "data": {},
+                                  'error': serializer.errors, 'meta': {}})
+
+        user = serializer.validated_data['user']
+        update_last_login(None, user)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response(status=status.HTTP_200_OK, data={"status": status.HTTP_200_OK, "Token": token.key, "meta": {}})
