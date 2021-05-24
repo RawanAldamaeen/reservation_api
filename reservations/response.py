f56c244e06cf95f7fbd9c4e0e54451e3592ea5ef
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-24 11:54:16 +0300

    modify doctor registration view

--- reservations/response.py
+++ reservations/response.py
@@ -1,13 +1,13 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 class Responses(APIView):
-    def getResponse(self, data, status, meta={}, msg=''):
+    def getResponse(status, data, meta={}, msg=''):     # return successful response
         return Response(status=status,
                         data={"status": status, "data": data, 'message': msg, 'meta': meta}, )
 
-    def getErrorResponse(self, status, error, data={}, meta={},  msg=''):
+    def getErrorResponse(status, error, data={}, meta={},  msg=''):     # return error response
         return Response(status=status,
                         data={"status": status, 'data': data, "error": error, 'message': msg, 'meta': meta}, )
-
