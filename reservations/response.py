94597f7e14e5f191d7fa22ed043fa2aa1ad6e54b
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-24 12:46:16 +0300

    fix conflicts

--- reservations/response.py
+++ reservations/response.py
@@ -1,3 +1,4 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -5,9 +6,8 @@ from rest_framework.response import Response
 class Responses(APIView):
     def getResponse(status, data, meta={}, msg=''):     # return successful response
         return Response(status=status,
-                        data={"status": status, "data": data, 'message': msg, 'meta': meta}, )
+                        data={"status": status, "data": data, 'message': msg, 'meta': meta},)
 
     def getErrorResponse(status, error, data={}, meta={},  msg=''):     # return error response
         return Response(status=status,
                         data={"status": status, 'data': data, "error": error, 'message': msg, 'meta': meta}, )
-
