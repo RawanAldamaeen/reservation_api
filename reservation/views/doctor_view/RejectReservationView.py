4302fb8ec84624e95d244a5907fa6bcb9842d2da
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-26 07:16:46 +0300

    add doctor reject reservation serializer and view

--- reservation/views/doctor_view/RejectReservationView.py
+++ reservation/views/doctor_view/RejectReservationView.py
@@ -10,14 +10,14 @@ from base.models.user import User
 from django.utils import translation
 
 
-def send_confirm_message(email):        # send confirm email function
+def send_reject_message(email, reason):        # send reject email function
     user = User.objects.get(email=email)
     patient_language = user.language
     translation.activate(patient_language)
 
-    email_title = _('Reservation Confirmed')
+    email_title = _('Reservation rejected')
     email_message = _(
-        'Hello, your reservation confirmed, be sure to be on the hospital on time')
+        f'Hello, your reservation is rejected, for the reason: {reason}, please choose another time')
     subject = email_title
     message = email_message
     email_from = settings.EMAIL_HOST_USER
@@ -25,13 +25,14 @@ def send_confirm_message(email):        # send confirm email function
     send_mail(subject, message, email_from, recipient_list)
 
 
-class ConfirmReservationView(APIView):  # confirm patient reservation view
+class RejectReservationView(APIView):  # reject patient reservation view
     queryset = Reservation.objects.all()
     serializer_class = ConfirmReservationSerializer
     authentication_classes = (authentication.TokenAuthentication,)
     lookup_field = 'pk'
 
     def put(self, request, *args, **kwargs):  # Perform PUT request
+        data = request.data
         pk = self.kwargs.get('pk')
         reservation = Reservation.objects.get(pk=pk)
 
@@ -39,10 +40,14 @@ class ConfirmReservationView(APIView):  # confirm patient reservation view
         if request.user.is_anonymous or not request.user.is_doctor or not reservation.doctor_id == request.user.doctor:
             return Responses.getErrorResponse(status=status.HTTP_403_FORBIDDEN,
                                               error=_('your not allowed to perform this action'))
+        if not data:
+            return Responses.getErrorResponse(status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                                              error=_('rejection reason field is required'))
 
-        reservation.status = 'confirm'
+        reservation.status = 'canceled'
+        reservation.rejection_reason = data['rejection_reason']
         reservation.save()
         serializer = ConfirmReservationSerializer(reservation)
         email = reservation.patient_id.user.email
-        send_confirm_message(email)
+        send_reject_message(email, reservation.rejection_reason)
         return Responses.getResponse(status=status.HTTP_200_OK, data=serializer.data)
