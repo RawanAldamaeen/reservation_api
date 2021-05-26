5e570ce6844f17c577614a084f52a114db2629a5
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-26 05:35:52 +0300

    add doctor confirm reservation serializer and view

--- reservation/serializers/doctor_serializer/ReservationListSerializer.py
+++ reservation/serializers/doctor_serializer/ReservationListSerializer.py
@@ -1,4 +1,3 @@
-
 from rest_framework import serializers
 from reservation.models.reservation import Reservation
 from patient.models.patient import Patient
@@ -11,8 +10,20 @@ class PatientName(serializers.ModelSerializer):  # class to return patient name
 
 
 class ReservationListSerializer(serializers.ModelSerializer):  # list of reservations serializer
+
     patient_id = PatientName(many=False, read_only=False)
+    actions = serializers.SerializerMethodField()
 
     class Meta:
         model = Reservation
-        fields = ['patient_id', 'time', 'date', 'status']
+        fields = ['id', 'patient_id', 'time', 'date', 'status', 'actions']
+
+    def get_actions(self, obj):
+        if obj.status == 'new':
+            status_choices = ('confirm', 'canceled')
+        elif obj.status == 'confirm':
+            status_choices = ('canceled', 'closed')
+        else:
+            status_choices = 'closed'
+
+        return status_choices
--- reservation/urls.py
+++ reservation/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views.patient_view import DoctorListView, NewReservationView
-from .views.doctor_view import ReservationListView
+from .views.doctor_view import ReservationListView, ConfirmReservationView
+
 urlpatterns = [
     path('reservation/new', NewReservationView.ReservationCreateView.as_view()),
     path('reservation/doctor-list', DoctorListView.DoctorsListView.as_view()),
     path('reservation/reservation-list', ReservationListView.ReservationsListView.as_view()),
+    path('reservation/<int:pk>/confirm', ConfirmReservationView.ConfirmReservationView.as_view()),
 
 ]
 
